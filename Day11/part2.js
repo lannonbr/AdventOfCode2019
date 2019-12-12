6b8e7d7ea71de14b0f2ce2f19aef41e38ee074cc
Benjamin Lannon <benjamin@lannonbr.com>
2019-12-11 19:37:43 -0500

    Day 11 - Space Police

---
+++ Day11/part2.js
@@ -0,0 +1,199 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.promises.readFile(path.join(__dirname, "input.txt")).then(buf => {
+  let program = buf
+    .toString()
+    .split(",")
+    .map(Number);
+
+  let oldSize = program.length;
+
+  program.length = oldSize * 10;
+
+  for (let i = oldSize; i < program.length; i++) {
+    program[i] = 0;
+  }
+
+  let i = 0;
+
+  let x = 0;
+  let y = 10;
+
+  let paintedPoints = [];
+
+  let dir = 0;
+  // 0 -> north (y++)
+  // 1 -> east (x++)
+  // 2 -> south (y--)
+  // 3 -> west (x--)
+
+  let grid = [];
+  let xSize = 43;
+  let ySize = 20;
+
+  for (let i_ = 0; i_ < xSize; i_++) {
+    grid[i_] = [];
+    grid[i_].length = ySize;
+    for (let j = 0; j < ySize; j++) {
+      grid[i_][j] = 0;
+    }
+  }
+
+  grid[x][y] = 1;
+
+  let outputCount = 0;
+  let relativePointer = 0;
+
+  while (program[i] !== 99) {
+    let { param1Mode, param2Mode, param3Mode, op } = parseOpCodeAndModes(
+      program[i]
+    );
+
+    // 0 = position mode, if pos[i] === 4, go get value at index 4
+    // 1 = immediate mode, if pos[i] === 4, use the literal value 4
+    // 2 = relative mode, if pos[i] === 4 and relativePointer is 4, go get value at index 4+4 -> 8
+    let param1, param2;
+
+    switch (param1Mode) {
+      case 0:
+        param1 = program[program[i + 1]];
+        break;
+      case 1:
+        param1 = program[i + 1];
+        break;
+      case 2:
+        param1 = program[relativePointer + program[i + 1]];
+        break;
+    }
+
+    switch (param2Mode) {
+      case 0:
+        param2 = program[program[i + 2]];
+        break;
+      case 1:
+        param2 = program[i + 2];
+        break;
+      case 2:
+        param2 = program[relativePointer + program[i + 2]];
+        break;
+    }
+
+    if (op === 1) {
+      // Add
+      // console.log('ADD', param1, param2, program[i+3])
+      program[program[i + 3] + (param3Mode ? relativePointer : 0)] =
+        param1 + param2;
+      i += 4;
+    } else if (op === 2) {
+      // Multiply
+      program[program[i + 3] + (param3Mode ? relativePointer : 0)] =
+        param1 * param2;
+      i += 4;
+    } else if (op === 3) {
+      // Store
+      if (param1Mode == 0) {
+        program[program[i + 1]] = grid[x][y];
+      } else if (param1Mode == 2) {
+        program[relativePointer + program[i + 1]] = grid[x][y];
+      }
+      i += 2;
+    } else if (op === 4) {
+      // Output
+      // console.log(`[OUTPUT] ${param1}`);
+      if (outputCount % 2 === 0) {
+        // paint color
+        grid[x][y] = param1;
+        paintedPoints.push({ x, y, col: param1 });
+      } else {
+        // turn
+        if (param1 === 0) {
+          // turn left
+          if (dir === 0) {
+            dir = 3;
+          } else {
+            dir--;
+          }
+        } else {
+          // turn right
+          dir = (dir + 1) % 4;
+        }
+
+        // move forward 1 dependent on direction
+        if (dir === 0) {
+          y++;
+        } else if (dir === 1) {
+          x++;
+        } else if (dir === 2) {
+          y--;
+        } else if (dir === 3) {
+          x--;
+        }
+      }
+      outputCount++;
+      i += 2;
+    } else if (op === 5) {
+      // Jump if non-zero
+      if (param1 !== 0) {
+        i = param2;
+      } else {
+        i += 3;
+      }
+    } else if (op === 6) {
+      // jump if 0
+      if (param1 === 0) {
+        i = param2;
+      } else {
+        i += 3;
+      }
+    } else if (op === 7) {
+      // less-than
+      if (param1 < param2) {
+        program[program[i + 3] + (param3Mode ? relativePointer : 0)] = 1;
+      } else {
+        program[program[i + 3] + (param3Mode ? relativePointer : 0)] = 0;
+      }
+      i += 4;
+    } else if (op === 8) {
+      // equals
+      if (param1 === param2) {
+        program[program[i + 3] + (param3Mode ? relativePointer : 0)] = 1;
+      } else {
+        program[program[i + 3] + (param3Mode ? relativePointer : 0)] = 0;
+      }
+      i += 4;
+    } else if (op === 9) {
+      relativePointer += param1;
+      i += 2;
+    }
+  }
+
+  let set = new Set(
+    paintedPoints.map(p => {
+      return `${p.x},${p.y}`;
+    })
+  );
+
+  for (let f = 0; f < xSize; f++) {
+    for (let g = 0; g < ySize; g++) {
+      if (grid[f][g] === 0) grid[f][g] = " ";
+      if (grid[f][g] === 1) grid[f][g] = "█";
+    }
+  }
+
+  console.log(grid.map(f => f.join("")));
+});
+
+function parseOpCodeAndModes(num) {
+  let numStr = num.toString();
+
+  numStr = numStr.padStart(5, "0");
+
+  let op = +numStr.slice(3);
+
+  let param1Mode = +numStr[2];
+  let param2Mode = +numStr[1];
+  let param3Mode = +numStr[0];
+
+  return { op, param1Mode, param2Mode, param3Mode };
+}
